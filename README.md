3f1f4d08688853112a7e0edf1ef588e48fbad763
allen218 <wangcongccnp@126.com>
2016-03-30 10:52:37 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+此源码并非本人所写，源码的作者为《Android开发进阶》何红辉老师所著。只是为了在自己博客上分析此源码，故上传到github。  
+项目源址：https://github.com/bboyfeiyu/android_my_pull_refresh_view 
---
+++ app/src/main/java/com/example/mac/pulltorefresh/MainActivity.java
@@ -0,0 +1,111 @@
+/**
+ *
+ *	created by Mr.Simple, Sep 12, 201411:18:06 AM.
+ *	Copyright (c) 2014, hehonghui@umeng.com All Rights Reserved.
+ *
+ *                #####################################################
+ *                #                                                   #
+ *                #                       _oo0oo_                     #   
+ *                #                      o8888888o                    #
+ *                #                      88" . "88                    #
+ *                #                      (| -_- |)                    #
+ *                #                      0\  =  /0                    #   
+ *                #                    ___/`---'\___                  #
+ *                #                  .' \\|     |# '.                 #
+ *                #                 / \\|||  :  |||# \                #
+ *                #                / _||||| -:- |||||- \              #
+ *                #               |   | \\\  -  #/ |   |              #
+ *                #               | \_|  ''\---/''  |_/ |             #
+ *                #               \  .-\__  '-'  ___/-. /             #
+ *                #             ___'. .'  /--.--\  `. .'___           #
+ *                #          ."" '<  `.___\_<|>_/___.' >' "".         #
+ *                #         | | :  `- \`.;`\ _ /`;.`/ - ` : | |       #
+ *                #         \  \ `_.   \_ __\ /__ _/   .-` /  /       #
+ *                #     =====`-.____`.___ \_____/___.-`___.-'=====    #
+ *                #                       `=---='                     #
+ *                #     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   #
+ *                #                                                   #
+ *                #               佛祖保佑         永无BUG              #
+ *                #                                                   #
+ *                #####################################################
+ */
+
+package com.example.mac.pulltorefresh;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ListView;
+
+import com.example.pulltorefreshview.base.impl.PullRefreshListView;
+
+
+/**
+ * @author mrsimple
+ */
+public class MainActivity extends Activity {
+
+    ListView mListView;
+
+    PullRefreshListView mPullRefreshListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pull_refresh_main);
+
+        findViewById(R.id.refresh_listview).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShowActivity.class);
+                intent.putExtra("index", ShowActivity.REFRESH_LV);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.refresh_gridview).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShowActivity.class);
+                intent.putExtra("index", ShowActivity.REFRESH_GV);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.refresh_textview).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShowActivity.class);
+                intent.putExtra("index", ShowActivity.REFRESH_TV);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.refresh_slide_lv).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShowActivity.class);
+                intent.putExtra("index", ShowActivity.REFRESH_SLIDE_LV);
+                startActivity(intent);
+            }
+        });
+
+        // swipe layout
+        findViewById(R.id.swipe_refresh_lv).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShowActivity.class);
+                intent.putExtra("index", ShowActivity.SWIPE_LV);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
